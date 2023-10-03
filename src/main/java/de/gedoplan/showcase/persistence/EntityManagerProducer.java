c9f744963455aba291e49d297d53fcd87389411e
Dirk Weil <dirk.weil@gedoplan.de>
2023-10-03 22:27:29 +0200

    Add Quarkus support

--- src/main/java/de/gedoplan/showcase/persistence/EntityManagerProducer.java
+++ src/main/java/de/gedoplan/showcase/persistence/EntityManagerProducer.java
@@ -5,6 +5,8 @@ import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+// This EntityManager producer is used in JEE applications only - it is ignored for Quarkus
+
 @ApplicationScoped
 public class EntityManagerProducer {
 
